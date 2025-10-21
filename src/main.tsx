87b1a089a4576f0152d4ff7a9b0b9a87639955b6
GuillermoJCV <memo92975@gmail.com>
2025-10-21 14:09:50 -0600

    Change alias

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Provider from "@/chakra-ui/provider";
-import App from "@/App";
+import Provider from "@/components/chakra-ui/provider";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
