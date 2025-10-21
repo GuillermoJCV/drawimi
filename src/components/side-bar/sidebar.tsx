7b0d4820cf1b3f177884d50e68099d84e249eaba
GuillermoJCV <memo92975@gmail.com>
2025-10-21 17:40:50 -0600

    add the sidebar

---
+++ src/components/side-bar/sidebar.tsx
@@ -0,0 +1,23 @@
+import { VStack, Avatar, Separator, Spacer } from "@chakra-ui/react";
+import { ColorModeButton } from "@/components/chakra-ui/color-mode";
+import CustomColorPicker from "./color-picker";
+import CustomBrushPicker from "./brush-picker";
+import CustomConfigSetter from "./config-setter";
+
+function SideBar() {
+  return (
+    <VStack as="aside" maxW="2xs" gap="2rem" h="dvh" pt="8" pb="8">
+      <Avatar.Root variant="subtle" colorPalette="green" w="3rem" h="3rem">
+        <Avatar.Fallback name="Jasmine Diaz" />
+      </Avatar.Root>
+      <Separator w="8rem" />
+      <CustomColorPicker />
+      <CustomBrushPicker />
+      <Spacer />
+      <ColorModeButton rounded="full" variant="subtle" colorPalette="green" />
+      <CustomConfigSetter />
+    </VStack>
+  );
+}
+
+export default SideBar;
