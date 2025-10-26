b7aa7273289328eaa62e4455dedf40f03a0c37c9
GuillermoJCV <memo92975@gmail.com>
2025-10-26 14:53:11 -0600

    Fix the zustand store

--- src/stores/brush-config-store.ts
+++ src/stores/brush-config-store.ts
@@ -1,25 +1,34 @@
 import { create, type StoreApi } from "zustand";
-import { type StyleInput, type ColorSource } from "pixi.js";
+import { type ColorSource, type StrokeInput } from "pixi.js";
 
-interface BrushConfig {
-  config: StyleInput;
-  setConfig: (newConfig: StyleInput) => void;
+type Config = {
+  config: StrokeInput;
+};
+
+type Actions = {
+  setConfig: (newConfig: Partial<StrokeInput>) => void;
   setColor: (color: ColorSource) => void;
   setWidth: (width: number) => void;
-}
+};
 
-const useBrushConfig: StoreApi<BrushConfig> = create((set) => ({
+const useBrushConfig: StoreApi<Config & Actions> = create((set) => ({
   config: {
     color: 0xff0000,
     width: 10,
     cap: "round",
   },
-  setConfig: (newConfig: StyleInput) =>
-    set((state) => Object.assign({}, state.config, newConfig)),
+  setConfig: (newConfig: Partial<StrokeInput>) =>
+    set((state) => ({
+      config: Object.assign({}, state.config, newConfig),
+    })),
   setColor: (color: ColorSource) =>
-    set((state) => Object.assign({}, state.config, color)),
+    set((state) => ({
+      config: Object.assign({}, state.config, { color }),
+    })),
   setWidth: (width: number) =>
-    set((state) => Object.assign({}, state.config, width)),
+    set((state) => ({
+      config: Object.assign({}, state.config, { width }),
+    })),
 }));
 
 export default useBrushConfig;
