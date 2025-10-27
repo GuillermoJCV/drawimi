60b45d2111eb95eae2333fcc16537a1857f3cd97
GuillermoJCV <memo92975@gmail.com>
2025-10-27 15:12:02 -0600

    Make the brush config updatable

--- src/components/side-bar/brush-picker.tsx
+++ src/components/side-bar/brush-picker.tsx
@@ -7,20 +7,29 @@ import {
   VStack,
   useSlider,
 } from "@chakra-ui/react";
+import { type ColorSource } from "pixi.js";
 import ColorPicker from "./brush-picker-components/color-picker";
 import WidthSlider from "./brush-picker-components/width-slider";
 import { RiBrushFill } from "react-icons/ri";
 import useBrushConfig from "@/stores/brush-config-store";
+import { useRef } from "react";
 
 function CustomBrushPicker() {
+  const setBrushColor = useBrushConfig((state) => state.setColor);
   const setBrushWidth = useBrushConfig((state) => state.setWidth);
+  const brushWidth: number | undefined = useBrushConfig(
+    (state) => state.config.width, //TODO: Type width is a number
+  );
+  const brushColor = useRef<ColorSource>("#86efac");
   const widthSlider = useSlider({
-    defaultValue: [20],
+    defaultValue: [brushWidth || 10],
     thumbAlignment: "center",
   });
 
   const updateBrush = () => {
     setBrushWidth(widthSlider.value[0]);
+    setBrushColor(brushColor.current);
+    console.log(brushColor.current);
   };
 
   return (
@@ -47,7 +56,7 @@ function CustomBrushPicker() {
             </Drawer.Header>
             <Drawer.Body>
               <VStack gap="2rem">
-                <ColorPicker />
+                <ColorPicker colorRef={brushColor} />
                 <WidthSlider slider={widthSlider} />
               </VStack>
             </Drawer.Body>
