4fab7c9786a44cd68332b750333f0980963340a1
GuillermoJCV <memo92975@gmail.com>
2025-10-27 15:09:09 -0600

    Add control to the color component

--- src/components/side-bar/brush-picker-components/color-picker.tsx
+++ src/components/side-bar/brush-picker-components/color-picker.tsx
@@ -1,8 +1,18 @@
 import { ColorPicker, HStack, Portal, parseColor } from "@chakra-ui/react";
+import { type RefObject } from "react";
+import { type ColorSource } from "pixi.js";
 
-function CustomColorPicker() {
+type Args = {
+  colorRef: RefObject<ColorSource>;
+};
+
+function CustomColorPicker({ colorRef }: Args) {
   return (
-    <ColorPicker.Root defaultValue={parseColor("#86efac")} maxW="200px">
+    <ColorPicker.Root
+      defaultValue={parseColor(colorRef.current.toString())}
+      onValueChange={(e) => (colorRef.current = e.value.toString("hex"))}
+      maxW="200px"
+    >
       <ColorPicker.HiddenInput />
       <ColorPicker.Control>
         <ColorPicker.Trigger data-fit-content rounded="full">
