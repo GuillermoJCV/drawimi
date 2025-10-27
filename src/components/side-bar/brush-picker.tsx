e17ac1e41072ea565ce51541bbd71729125f30d1
GuillermoJCV <memo92975@gmail.com>
2025-10-26 19:11:04 -0600

    Add the color picker, and the width slider. Add the brush update functionality

--- src/components/side-bar/brush-picker.tsx
+++ src/components/side-bar/brush-picker.tsx
@@ -2,13 +2,27 @@ import {
   Drawer,
   Button,
   CloseButton,
-  Kbd,
   Portal,
   IconButton,
+  VStack,
+  useSlider,
 } from "@chakra-ui/react";
+import ColorPicker from "./brush-picker-components/color-picker";
+import WidthSlider from "./brush-picker-components/width-slider";
 import { RiBrushFill } from "react-icons/ri";
+import useBrushConfig from "@/stores/brush-config-store";
 
 function CustomBrushPicker() {
+  const setBrushWidth = useBrushConfig((state) => state.setWidth);
+  const widthSlider = useSlider({
+    defaultValue: [20],
+    thumbAlignment: "center",
+  });
+
+  const updateBrush = () => {
+    setBrushWidth(widthSlider.value[0]);
+  };
+
   return (
     <Drawer.Root size="xs">
       <Drawer.Trigger asChild>
@@ -29,16 +43,21 @@ function CustomBrushPicker() {
         <Drawer.Positioner>
           <Drawer.Content>
             <Drawer.Header>
-              <Drawer.Title>Drawer Title</Drawer.Title>
+              <Drawer.Title>Brush Config</Drawer.Title>
             </Drawer.Header>
             <Drawer.Body>
-              Press the <Kbd>esc</Kbd> key to close the drawer.
+              <VStack gap="2rem">
+                <ColorPicker />
+                <WidthSlider slider={widthSlider} />
+              </VStack>
             </Drawer.Body>
             <Drawer.Footer>
               <Drawer.ActionTrigger asChild>
                 <Button variant="outline">Cancel</Button>
               </Drawer.ActionTrigger>
-              <Button>Save</Button>
+              <Drawer.ActionTrigger asChild>
+                <Button onClick={updateBrush}>Save</Button>
+              </Drawer.ActionTrigger>
             </Drawer.Footer>
             <Drawer.CloseTrigger asChild>
               <CloseButton size="sm" />
