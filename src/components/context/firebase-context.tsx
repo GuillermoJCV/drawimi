9abce2333dd505c2d340ee6e339217ec86bdb3bf
GuillermoJCV <memo92975@gmail.com>
2025-10-28 16:08:33 -0600

    Add a global context to get firebase info

--- src/components/context/firebase-context.tsx
+++ src/components/context/firebase-context.tsx
@@ -5,7 +5,6 @@ const firebaseConfig = {
   apiKey: "AIzaSyCB42qRE98FUjcDVYczfANRhRwIEnuSawE",
   authDomain: "drawimi.firebaseapp.com",
   projectId: "drawimi",
-  storageBucket: "drawimi.firebasestorage.app",
   messagingSenderId: "366986843739",
   appId: "1:366986843739:web:08089223549fd4dd6d517e",
 };
