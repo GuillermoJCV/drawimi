9ca47be94e1755184f3452c6e28cabe423e071e7
GuillermoJCV <memo92975@gmail.com>
2025-10-31 16:20:36 -0600

    Setup of the toaster

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Container, HStack } from "@chakra-ui/react";
 import SideBar from "@/components/side-bar/sidebar";
 import CanvasPaint from "@/components/canvas/canvas";
+import { Toaster } from "@/components/chakra-ui/toaster";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <SideBar />
         <CanvasPaint />
       </HStack>
+      <Toaster />
     </Container>
   );
 }
