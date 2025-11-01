0a713c761f744e683610fa76694390ca45d93b49
GuillermoJCV <memo92975@gmail.com>
2025-10-31 18:18:00 -0600

    Handle the different auth providers

--- src/components/side-bar/user-config/sign-in.tsx
+++ src/components/side-bar/user-config/sign-in.tsx
@@ -1,7 +1,13 @@
 import { Button, Field, Input, Stack } from "@chakra-ui/react";
 import { PasswordInput } from "@/components/chakra-ui/password-input";
 import { useForm } from "react-hook-form";
-import { useSignInEmailPass } from "@/hooks/firebase";
+import {
+  useFacebookProvider,
+  useGoogleProvider,
+  useTwitterProvider,
+  useSignInEmailPass,
+} from "@/hooks/firebase";
+import { BsFacebook, BsGoogle, BsTwitter } from "react-icons/bs";
 
 interface FormValues {
   email: string;
@@ -9,6 +15,9 @@ interface FormValues {
 }
 
 function SignIn() {
+  const signWithGoogle = useGoogleProvider();
+  const signWithFacebook = useFacebookProvider();
+  const signWithTwitter = useTwitterProvider();
   const signIn = useSignInEmailPass();
   const {
     register,
@@ -22,20 +31,34 @@ function SignIn() {
 
   return (
     <form onSubmit={onSubmit}>
-      <Stack gap="4" align="center" colorPalette="green">
-        <Field.Root invalid={!!errors.email}>
+      <Stack gap="4" align="center">
+        <Field.Root invalid={!!errors.email} colorPalette="green">
           <Field.Label>Username or Email</Field.Label>
           <Input {...register("email")} />
           <Field.ErrorText>{errors.email?.message}</Field.ErrorText>
         </Field.Root>
 
-        <Field.Root invalid={!!errors.password}>
+        <Field.Root invalid={!!errors.password} colorPalette="green">
           <Field.Label>Password</Field.Label>
           <PasswordInput {...register("password")} />
           <Field.ErrorText>{errors.password?.message}</Field.ErrorText>
         </Field.Root>
 
-        <Button type="submit">Sign Up</Button>
+        <Button type="submit" colorPalette="green">
+          Sign Up
+        </Button>
+        <Button colorPalette="white" w="full" onClick={() => signWithGoogle()}>
+          Sign with
+          <BsGoogle />
+        </Button>
+        <Button colorPalette="blue" w="full" onClick={() => signWithFacebook()}>
+          Sign with
+          <BsFacebook />
+        </Button>
+        <Button colorPalette="teal" w="full" onClick={() => signWithTwitter()}>
+          Sign with
+          <BsTwitter />
+        </Button>
       </Stack>
     </form>
   );
--- src/hooks/firebase.tsx
+++ src/hooks/firebase.tsx
@@ -2,15 +2,74 @@ import { useContext, useCallback } from "react";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
+  signInWithPopup,
 } from "firebase/auth";
 import { FirebaseContext } from "@/components/context/firebase-context";
-import userUserStore from "@/stores/user-store";
+import useUserStore from "@/stores/user-store";
 import { toaster } from "@/components/chakra-ui/toaster";
+import {
+  GoogleAuthProvider,
+  FacebookAuthProvider,
+  TwitterAuthProvider,
+} from "firebase/auth/web-extension";
 
-const handleAuthError = (err: any) => console.log(err);
+function useFacebookProvider() {
+  const setUser = useUserStore((state) => state.setUser);
+  const { auth } = useContext(FirebaseContext);
+  const provider = new FacebookAuthProvider();
+
+  const useAuth = useCallback(async () => {
+    const promise = signInWithPopup(auth, provider)
+      .then((result) => {
+        setUser(result.user);
+      })
+      .catch(handleAuthError);
+
+    toaster.promise(promise, toasterConfig);
+  }, []);
+
+  return useAuth;
+}
+
+function useTwitterProvider() {
+  const setUser = useUserStore((state) => state.setUser);
+  const { auth } = useContext(FirebaseContext);
+  const provider = new TwitterAuthProvider();
+
+  const useAuth = useCallback(async () => {
+    const promise = signInWithPopup(auth, provider)
+      .then((result) => {
+        setUser(result.user);
+      })
+      .catch(handleAuthError);
+
+    toaster.promise(promise, toasterConfig);
+  }, []);
+
+  return useAuth;
+}
+
+function useGoogleProvider() {
+  const setUser = useUserStore((state) => state.setUser);
+  const { auth } = useContext(FirebaseContext);
+  const provider = new GoogleAuthProvider();
+  provider.addScope("https://www.googleapis.com/auth/contacts.readonly");
+
+  const useAuth = useCallback(async () => {
+    const promise = signInWithPopup(auth, provider)
+      .then((result) => {
+        setUser(result.user);
+      })
+      .catch(handleAuthError);
+
+    toaster.promise(promise, toasterConfig);
+  }, []);
+
+  return useAuth;
+}
 
 function useCreateEmailPass() {
-  const setUser = userUserStore((state) => state.setUser);
+  const setUser = useUserStore((state) => state.setUser);
   const { auth } = useContext(FirebaseContext);
 
   const useAuth = useCallback(async (email: string, password: string) => {
@@ -20,24 +79,14 @@ function useCreateEmailPass() {
       })
       .catch(handleAuthError);
 
-    toaster.promise(promise, {
-      success: {
-        title: "Successfully signed!",
-        description: "Welcome to Drawimi",
-      },
-      error: {
-        title: "Failed siging in",
-        description: "Try again later",
-      },
-      loading: { title: "Signing...", description: "Please wait" },
-    });
+    toaster.promise(promise, toasterConfig);
   }, []);
 
   return useAuth;
 }
 
 function useSignInEmailPass() {
-  const setUser = userUserStore((state) => state.setUser);
+  const setUser = useUserStore((state) => state.setUser);
   const { auth } = useContext(FirebaseContext);
 
   const useAuth = useCallback(async (email: string, password: string) => {
@@ -47,20 +96,30 @@ function useSignInEmailPass() {
       })
       .catch(handleAuthError);
 
-    toaster.promise(promise, {
-      success: {
-        title: "Successfully signed!",
-        description: "Welcome to Drawimi",
-      },
-      error: {
-        title: "Failed siging in",
-        description: "Try again later",
-      },
-      loading: { title: "Signing...", description: "Please wait" },
-    });
+    toaster.promise(promise, toasterConfig);
   }, []);
 
   return useAuth;
 }
 
-export { useCreateEmailPass, useSignInEmailPass };
+export {
+  useCreateEmailPass,
+  useSignInEmailPass,
+  useGoogleProvider,
+  useFacebookProvider,
+  useTwitterProvider,
+};
+
+const handleAuthError = (err: any) => console.log(err);
+
+const toasterConfig = {
+  success: {
+    title: "Successfully signed!",
+    description: "Welcome to Drawimi",
+  },
+  error: {
+    title: "Failed siging in",
+    description: "Try again later",
+  },
+  loading: { title: "Signing...", description: "Please wait" },
+};
