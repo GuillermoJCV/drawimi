5eaa84fecd9892b1f726d013c79152ee77fd9a70
GuillermoJCV <memo92975@gmail.com>
2025-11-03 16:53:30 -0600

    Create types for downloading options

---
+++ src/constants/sidebar/download-formats.ts
@@ -0,0 +1,6 @@
+const downloadFormats = [".png", ".jpeg", ".webp"] as const;
+
+type DownloadFormat = (typeof downloadFormats)[number];
+
+export { downloadFormats };
+export type { DownloadFormat };
---
+++ src/types/canvas/download.ts
@@ -0,0 +1,8 @@
+import { DownloadFormat } from "@/constants/sidebar/download-formats";
+
+type DownloadOptions = {
+  filename: string;
+  format: DownloadFormat;
+};
+
+export type { DownloadOptions };
