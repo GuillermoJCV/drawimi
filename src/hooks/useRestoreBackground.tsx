a417a2baaa9df9def9b37aa36ec908e7473f33e2
GuillermoJCV <memo92975@gmail.com>
2025-11-03 16:17:02 -0600

    Fix the image disappearings when changing the brush

--- src/hooks/useRestoreBackground.tsx
+++ src/hooks/useRestoreBackground.tsx
@@ -1,32 +1,31 @@
-import { useState, useCallback } from "react";
-
 //TODO: Store the background on a zustand array
+let restoreImage: ImageData | null = null;
 function useRestoreBackground(
   ctx: CanvasRenderingContext2D | null,
   canvasRef: React.RefObject<HTMLCanvasElement | null>,
 ) {
   //Use a zustand state
-  const [restoreImage, setRestoreImage] = useState<ImageData | null>(null);
 
   const restoreBackground = () => {
     if (!ctx || !restoreImage || !canvasRef.current) return;
+    console.log("restoring the Background");
 
     const size = canvasRef.current.getBoundingClientRect();
     ctx.putImageData(restoreImage, size.x, size.y);
   };
 
-  const storeBackground = useCallback(() => {
+  const storeBackground = () => {
     if (!ctx || !canvasRef.current) return;
+    console.log("storing the Background");
 
     const size = canvasRef.current.getBoundingClientRect();
-    const imageData = ctx.getImageData(
+    restoreImage = ctx.getImageData(
       size.left,
       size.top,
       size.right,
       size.bottom,
     );
-    setRestoreImage(imageData);
-  }, [canvasRef.current]);
+  };
 
   return {
     restoreBackground,
