b36193df1be7e1c648a8162ad2097a12f1e1d348
GuillermoJCV <memo92975@gmail.com>
2025-11-04 19:29:37 -0600

    Create a responsive sidebar (it will change)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Container, HStack } from "@chakra-ui/react";
+import { Container, Stack, type StackProps } from "@chakra-ui/react";
 import SideBar from "@/components/side-bar/sidebar";
 import CanvasPaint from "@/components/canvas/vanilla-container";
 import { Toaster } from "@/components/chakra-ui/toaster";
@@ -6,13 +6,21 @@ import { Toaster } from "@/components/chakra-ui/toaster";
 function App() {
   return (
     <Container as="main" w="dvw" h="dvh">
-      <HStack h="100%" pt="2rem" pb="2rem" pr="1rem" align="stretch">
+      <Stack {...stackProps}>
         <SideBar />
         <CanvasPaint />
-      </HStack>
+      </Stack>
       <Toaster />
     </Container>
   );
 }
+
+const stackProps: StackProps & React.RefAttributes<HTMLDivElement> = {
+  h: "100%",
+  p: { smDown: "1rem", md: "2rem" },
+  align: "center",
+  justify: "center",
+  direction: { smDown: "column", md: "row" },
+};
 
 export default App;
