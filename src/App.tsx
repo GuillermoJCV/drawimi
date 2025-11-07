2a0e46ade1294f0cb859c2152affc78d4ca7699e
GuillermoJCV <memo92975@gmail.com>
2025-11-06 19:04:01 -0600

    Fix the components view responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import { Container, Stack, type StackProps } from "@chakra-ui/react";
 import SideBar from "@/components/side-bar/sidebar";
 import CanvasPaint from "@/components/canvas/vanilla-container";
 import { Toaster } from "@/components/chakra-ui/toaster";
+import Header from "@/components/header/header";
+import Footer from "@/components/footer/footer";
 
 function App() {
   return (
     <Container as="main" w="dvw" h="dvh">
       <Stack {...stackProps}>
-        <SideBar />
+        <Header hideFrom="md" />
+        <SideBar hideBelow="sm" />
         <CanvasPaint />
+        <Footer hideFrom="md" />
       </Stack>
       <Toaster />
     </Container>
