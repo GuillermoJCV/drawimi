6a52fec4116b21d7d13a3b6faf8f95b8ebf99c1a
GuillermoJCV <memo92975@gmail.com>
2025-11-06 18:38:42 -0600

    Provide a more easy prop setter through components

--- src/components/side-bar/sidebar.tsx
+++ src/components/side-bar/sidebar.tsx
@@ -6,31 +6,39 @@ import DownloadOptionsPanel from "../utils/download-options";
 import CustomColorPicker from "../utils/brush-picker/color-picker";
 import User from "../utils/user-config";
 
-function SideBar() {
+type Args = Omit<StackProps & React.RefAttributes<HTMLDivElement>, "children">;
+
+function SideBar({
+  as = "aside",
+  maxW = { smDown: "100%", md: "2xs" },
+  gap = { smDown: "clamp(0.5rem, 5%, 2rem)", md: "2rem" },
+  h = { smDown: "fit-content", md: "100%" },
+  justify = "center",
+  align = "center",
+  direction = { smDown: "row", md: "column" },
+  ...props
+}: Args) {
   return (
-    //TODO: This component must be rendered just when the device is large
-    // So it doesn't need conditional props anymore
-    <Stack {...stackProps}>
+    <Stack
+      as={as}
+      maxW={maxW}
+      gap={gap}
+      h={h}
+      justify={justify}
+      align={align}
+      direction={direction}
+      {...props}
+    >
       <User />
-      <Separator w="8rem" hideBelow="sm" />
+      <Separator w="8rem" />
       <CustomBrushPickerPanel size="xs" />
       <CustomColorPicker needInput={false} />
-      <Spacer hideBelow="sm" />
+      <Spacer />
       <DownloadOptionsPanel size="xs" />
       <ColorModeButton rounded="full" variant="subtle" colorPalette="green" />
       <CustomConfigSetterPanel size="xs" />
     </Stack>
   );
 }
-
-const stackProps: StackProps & React.RefAttributes<HTMLDivElement> = {
-  as: "aside",
-  maxW: { smDown: "100%", md: "2xs" },
-  gap: { smDown: "clamp(0.5rem, 5%, 2rem)", md: "2rem" },
-  h: { smDown: "fit-content", md: "100%" },
-  justify: "center",
-  align: "center",
-  direction: { smDown: "row", md: "column" },
-};
 
 export default SideBar;
