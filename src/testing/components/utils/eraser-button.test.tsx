47a19619e555eb395c2aaf3f5ca918034cc566ad
GuillermoJCV <memo92975@gmail.com>
2025-11-12 19:00:31 -0600

    Add testing for eraser-button

--- src/testing/components/utils/eraser-button.test.tsx
+++ src/testing/components/utils/eraser-button.test.tsx
@@ -1,16 +1,13 @@
 import { screen } from "@testing-library/react";
-import { Button } from "@chakra-ui/react";
 import { expect } from "vitest";
-//import EraserButton, { eraserButtonTestId } from "./eraser-button";
 import { render } from "@/testing/render";
 import "@testing-library/jest-dom/vitest";
+import EraserButton, { TestId } from "@/components/utils/eraser-button";
 
-describe("Basic test", () => {
-  try {
-    it("works", () => {
-      expect(true).toBeTruthy();
-    });
-  } catch (err) {
-    console.error(err);
-  }
+describe("Eraser button", async () => {
+  it("renders", () => {
+    render(<EraserButton />);
+    const eraser = screen.getByTestId(TestId.ERASER_BUTTON);
+    expect(eraser).toBeInTheDocument();
+  });
 });
