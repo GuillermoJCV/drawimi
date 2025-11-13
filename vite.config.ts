3f0e015e0ae198124d6861f0a2ea85e915216de1
GuillermoJCV <memo92975@gmail.com>
2025-11-12 19:00:48 -0600

    Set configuration for vitest

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
-export default defineConfig(async () => ({
+export default defineConfig(() => ({
   plugins: [react(), tsconfigPaths()],
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,38 +1,15 @@
-import { defineConfig } from "vitest/config";
-import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig, mergeConfig } from "vitest/config";
+import viteConfig from "./vite.config";
 
-const host = process.env.TAURI_DEV_HOST;
-
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  test: {
-    environment: "jsdom",
-    globals: true,
-    setupFiles: "./setup-test.ts",
-    pool: "forks",
-    // server: {
-    //   deps: {
-    //     inline: ["parse5"],
-    //   },
-    // },
-  },
-  clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
-  server: {
-    port: 1420,
-    strictPort: true,
-    host: host || false,
-    hmr: host
-      ? {
-          protocol: "ws",
-          host,
-          port: 1421,
-        }
-      : undefined,
-    watch: {
-      // 3. tell Vite to ignore watching `src-tauri`
-      ignored: ["**/src-tauri/**"],
-    },
-  },
-});
+export default defineConfig((configEnv) =>
+  mergeConfig(
+    viteConfig(configEnv),
+    defineConfig({
+      test: {
+        environment: "happy-dom",
+        globals: true,
+        setupFiles: ["./__mooks__/window.ts", "./__mooks__/zustand.ts"],
+      },
+    }),
+  ),
+);
