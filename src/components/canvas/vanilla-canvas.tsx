826a10756f715ce8e6c4e499660e9295a1b9600e
GuillermoJCV <memo92975@gmail.com>
2025-11-26 19:47:33 -0600

    Add a code to handle the first background store

--- src/components/canvas/vanilla-canvas.tsx
+++ src/components/canvas/vanilla-canvas.tsx
@@ -2,15 +2,16 @@ import useCanvasHandler from "@/hooks/canvas/useCanvasHandler";
 import { useRestoreBackground } from "@/hooks/canvas/useRestoreBackground";
 import useBrushConfig from "@/stores/brush-config-store";
 import useCanvasStore from "@/stores/canvas-store";
+import useBackgroundStore from "@/stores/storeBackgrounds";
 import { useEffect, useRef, useState } from "react";
 
 interface Args {
   resizeTo: DOMRect;
 }
 
-//TODO: The draw disappear when resizing
 function Canvas({ resizeTo }: Args) {
   const brushConfig = useBrushConfig((state) => state.config);
+  const getImage = useBackgroundStore((state) => state.getImage);
   const setCanvas = useCanvasStore((state) => state.setCanvas);
   const [ctx, setCtx] = useState<CanvasCtx | null>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -34,11 +35,20 @@ function Canvas({ resizeTo }: Args) {
       canvas.addEventListener("pointerup", stop);
       canvas.addEventListener("pointerout", stop);
       canvas.addEventListener("pointermove", draw);
+      // When this function is called canvas and ctx may be not setted
+      // Because setCanvas and setCtx contains asyncronous functions
+      // So the arguments must be passed through here
+      if (context && !getImage(0)) {
+        storeBackground({
+          definedCanvas: canvas,
+          definedCtx: context,
+          isInitialBackground: true,
+        });
+      }
     }
 
     return () => {
       if (canvas) {
-        storeBackground();
         canvas.removeEventListener("pointerdown", start);
         canvas.removeEventListener("pointerup", stop);
         canvas.removeEventListener("pointerout", stop);
@@ -47,7 +57,6 @@ function Canvas({ resizeTo }: Args) {
     };
   }, [canvasRef.current, brushConfig]);
 
-  // Canvas Element
   return (
     <canvas
       data-testid={TestId.CANVAS}
