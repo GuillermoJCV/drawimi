e721c4bfd8a7d589c69ca3cb8d9c3bc15f3663bf
GuillermoJCV <memo92975@gmail.com>
2025-11-26 19:45:52 -0600

    Fix the bug that cuts the image

--- src/hooks/canvas/useRestoreBackground.tsx
+++ src/hooks/canvas/useRestoreBackground.tsx
@@ -1,30 +1,56 @@
 import useCanvasStore from "@/stores/canvas-store";
+import useBackgroundStore from "@/stores/storeBackgrounds";
 import { useCallback } from "react";
 
-//TODO: Store the background on a zustand array
-let restoreImage: ImageData | null = null;
 function useRestoreBackground() {
+  const getCurrentIndex = useBackgroundStore((state) => state.getCurrentIndex);
+  const getImage = useBackgroundStore((state) => state.getImage);
+  const push = useBackgroundStore((state) => state.push);
+  const storeFirstBackground = useBackgroundStore(
+    (state) => state.storeFirstBackground,
+  );
+
   const canvas = useCanvasStore((state) => state.canvas);
   const ctx = canvas?.getContext("2d");
 
   const restoreBackground = useCallback(() => {
-    if (!ctx || !restoreImage || !canvas) return;
+    const currentIndex = getCurrentIndex();
+    const currentImage = getImage(currentIndex);
+    if (!ctx || currentIndex < 0 || !canvas) return;
+    else if (!currentImage) return;
 
-    const size = canvas.getBoundingClientRect();
-    ctx.putImageData(restoreImage, size.x, size.y);
+    ctx.putImageData(currentImage, 0, 0);
   }, [canvas]);
 
-  const storeBackground = useCallback(() => {
-    if (!ctx || !canvas) return;
-    const size = canvas.getBoundingClientRect();
-    if (size.x === 0) return;
-    restoreImage = ctx.getImageData(
-      size.left,
-      size.top,
-      size.right,
-      size.bottom,
-    );
-  }, [canvas]);
+  const storeBackground = useCallback(
+    async (args?: StoreBackgroundArguments) => {
+      if ((!ctx || !canvas) && (!args?.definedCanvas || !args?.definedCtx))
+        return;
+
+      const currentCanvas = canvas
+        ? canvas
+        : (args?.definedCanvas as HTMLCanvasElement);
+      const currentCtx = ctx
+        ? ctx
+        : (args?.definedCtx as CanvasRenderingContext2D);
+
+      const size = currentCanvas.getBoundingClientRect();
+      if (size.x === 0) return;
+      const currentImage = currentCtx.getImageData(
+        0,
+        0,
+        currentCanvas.width,
+        currentCanvas.height,
+      );
+      console.log("\nSTORE IMAGE");
+      console.log(size);
+      console.log(currentImage);
+      console.log("\nSTORE IMAGE");
+      if (!args?.isInitialBackground) push(currentImage);
+      else storeFirstBackground(currentImage);
+    },
+    [canvas],
+  );
 
   return {
     restoreBackground,
@@ -33,3 +59,10 @@ function useRestoreBackground() {
 }
 
 export { useRestoreBackground };
+export type { StoreBackgroundArguments };
+
+type StoreBackgroundArguments = {
+  definedCtx: CanvasRenderingContext2D;
+  definedCanvas: HTMLCanvasElement;
+  isInitialBackground: boolean;
+};
