9ed8c3f20982410096db4362b2932a46cc480e18
GuillermoJCV <memo92975@gmail.com>
2025-12-15 00:41:23 -0600

    Expect more emulators on future

--- src/components/context/firebase-context.tsx
+++ src/components/context/firebase-context.tsx
@@ -1,6 +1,7 @@
 import { PropsWithChildren, createContext } from "react";
 import { FirebaseApp, initializeApp } from "firebase/app";
 import { connectAuthEmulator, getAuth, type Auth } from "firebase/auth";
+import { Firestore, getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCB42qRE98FUjcDVYczfANRhRwIEnuSawE",
@@ -11,17 +12,22 @@ const firebaseConfig = {
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-import.meta.env.MODE == "test" &&
+const db = getFirestore(app);
+
+if (import.meta.env.MODE == "test") {
   connectAuthEmulator(auth, "http://127.0.0.1:9099");
+}
 
 type FirebaseContextValues = {
   app: FirebaseApp;
   auth: Auth;
+  db: Firestore;
 };
 
 const defaultValues: FirebaseContextValues = {
   app,
   auth,
+  db,
 };
 
 const FirebaseContext = createContext<FirebaseContextValues>(defaultValues);
